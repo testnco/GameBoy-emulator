6b5ac9e983a49484c8cb9dee925fcf38dac11f70
testnco <45320002+testnco@users.noreply.github.com>
2023-12-01 21:22:20 -0500

    Create script for mac and modify script for windows

---
+++ mac_configure_project.sh
@@ -0,0 +1,3 @@
+rm -rf build/
+cmake -B build -DCMAKE_BUILD_TYPE=Release .
+echo "CMake configuration completed."
--- win_configure_project.bat
+++ win_configure_project.bat
@@ -1,3 +1,3 @@
 Remove-Item -Recurse -Force -Path "build"
-cmake -G "MinGW Makefiles"  -B build -DCMAKE_BUILD_TYPE=Release -DMINGW=1 .
+cmake -G "MinGW Makefiles"  -B build -DCMAKE_BUILD_TYPE=Release .
 echo "CMake configuration completed."
